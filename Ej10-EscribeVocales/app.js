28fca315e9e359b336916d5fa7ea162ad9345bb2
raulblas7 <48948743+raulblas7@users.noreply.github.com>
2021-08-05 01:27:51 +0200

    MOD: Mejor lo hago con una funcion

--- Ej10-EscribeVocales/app.js
+++ Ej10-EscribeVocales/app.js
@@ -4,32 +4,28 @@ var s = prompt("Escriba una cadena de texto y escribire solo las vocales:");
 
 // busca todas las iteraciones de la letra a y las almacena en un array gracias a la letra g
 // la i es para que de igual si es mayuscula o minuscula
+function escribe(total, resultado){
+    for(let i = 0; i < total; i++){
+        document.write(resultado[i]);
+    }
+}
+
 var resultadoA = s.match(/a/gi);
 var total = resultadoA.length;
-for(let i = 0; i < total; i++){
-    document.write(resultadoA[i]);
-}
+escribe(total, resultadoA);
 
 var resultadoE = s.match(/e/gi);
 total = resultadoE.length;
-for(let i = 0; i < total; i++){
-    document.write(resultadoE[i]);
-}
+escribe(total, resultadoE);
 
 var resultadoI = s.match(/i/gi);
 total = resultadoI.length;
-for(let i = 0; i < total; i++){
-    document.write(resultadoI[i]);
-}
+escribe(total, resultadoI);
 
 var resultadoO = s.match(/o/gi);
 total = resultadoO.length;
-for(let i = 0; i < total; i++){
-    document.write(resultadoO[i]);
-}
+escribe(total, resultadoO);
 
 var resultadoU = s.match(/u/gi);
 total = resultadoU.length;
-for(let i = 0; i < total; i++){
-    document.write(resultadoU[i]);
-}
+escribe(total, resultadoU);
